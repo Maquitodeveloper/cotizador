eaddf1020894ce7c02cb5a1d809bad1d5effc400
Alexander Guanchez <christianpaypal09@gmail.com>
2025-07-15 16:25:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//CREAMOS LOS SELECTORES
+const form = document.querySelector('#coin-form')
+const coin = document.querySelector('#coin')
+const crypto = document.querySelector('#crypto')
+const amount = document.querySelector('#amount')
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const coinSelected = [...coin.children].find(option => option.selected);
+});
+
